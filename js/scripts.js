0577fe3dc75f134db1d0d8ebe2c96fe08ae4c7ec
Erika Fleck <erika.fleck@gmail.com>
2013-11-06 14:51:12 -0800

    Base
    Basic styling of index, grid and list view, and added page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
 // JavaScript Document
 
 $(document).ready(function(){
+	//switch to grid view 
+	$('#grid').click(function() {
+		//hide paragraphs
+		$('p').hide();
+		//remove existing class
+		$('section').removeClass('col-md-6');
+		//add new class
+		$('section').addClass('col-md-3');
+		
+	});
 	
-	// Good Stuff
+		//switch to list view 
+	$('#list').click(function() {
+		//hide paragraphs
+		$('p').show();
+		//remove existing class
+		$('section').removeClass('col-md-3');
+		//add new class
+		$('section').addClass('col-md-6');
+		
+	});
+	
+	
 	
 });
